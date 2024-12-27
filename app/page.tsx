eaf58cb190301a504f0594a73933864d0b454b12
HJSunDev <13720980943@126.com>
2024-12-27 23:51:38 +0800

    feat(auth): integrate Clerk and add authentication examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       home page
+      <SignedOut>
+        <SignUpButton>
+          <button>Sign Up</button>
+        </SignUpButton>
+      </SignedOut>
+
+      <UserButton />
+
+      <SignedIn>
+        <SignOutButton>
+          <button>Sign Out</button>
+        </SignOutButton>
+      </SignedIn>
     </div>
   );
 }
